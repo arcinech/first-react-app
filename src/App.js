56a6abe8cea95579368601a65816d9246f8457b8
Marcin Lech <arcinech@gmail.com>
2022-05-04 21:07:56 +0200

    Add: Route for lists with listId

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NotFound from './pages/NotFound/NotFound';
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
 import {Routes, Route} from 'react-router-dom';
+import List from './components/List/List';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route path='/' element ={<Home />}/>
           <Route path='/about' element={<About />} />
           <Route path='/favorite' element={<Favorite />}/>
+          <Route path="/list/:listId" element={<List />} />
+
           <Route path='*' element={<NotFound />}/>
         </Routes>
       </Container>

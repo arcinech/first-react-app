a8ba49bd23b86db9e1f35a4f69015de28f6af772
Marcin Lech <arcinech@gmail.com>
2022-05-04 21:12:36 +0200

    Refresh to null state dispatch on initialization

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,7 +1,7 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { updateSearch } from '../../redux/store';
 
@@ -9,6 +9,11 @@ const SearchForm = () => {
   const [text, setText] = useState('');
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(updateSearch(''));
+
+  }, [dispatch])
+  
   const handleSearch = (e) => {
     e.preventDefault();
     dispatch(updateSearch(text));

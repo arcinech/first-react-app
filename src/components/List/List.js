25582e66bab2b8ab18f9a8d77af26755f3f9c0fd
Marcin Lech <arcinech@gmail.com>
2022-05-04 21:12:05 +0200

    Add: use of ListId

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,31 +1,38 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
-import ColumnForm from '../ColumnForm/ColumnForm'
+import ColumnForm from '../ColumnForm/ColumnForm';
+import SearchForm from '../SearchForm/SearchForm';
 import { useSelector } from 'react-redux';
-import { getAllColumns } from '../../redux/store';
+import { getColumnsByList, getListById } from '../../redux/store';
+import { useParams } from 'react-router';
 
 
 const List = () => {
-  const columns = useSelector(getAllColumns);
+
+  const { listId } = useParams();
+  const columns = useSelector(state => getColumnsByList(state, listId));
+  const listData = useSelector(state => getListById(state, listId));
 
   return (
     <div className= {styles.list}>
       <header className={styles.header}>
         <div>
-          <h2 className={styles.title}>Things to do <span>soon!</span></h2>
+          <h2 className={styles.title}>{listData.title}<span>soon!</span></h2>
         </div>
       </header>
         <div>
-          <p className={styles.description} >Interesting Things I want to check out</p>
+          <p className={styles.description} >{listData.description}</p>
         </div>
+      <SearchForm />
       <section className={styles.columns} >
         {columns.map(column => 
           <Column 
             key={column.id} 
-            {...column}/>
+            {...column}
+          />
         )}
       </section>
-        <ColumnForm /> 
+        <ColumnForm listId={listId}/> 
     </div>
   )
 };

67d6687ba741cb9c06c5eabd6ff41b7cf404adf4
Marcin Lech <arcinech@gmail.com>
2022-05-04 21:11:38 +0200

    Revert changes to NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,39 +2,15 @@ import {NavLink, Link} from 'react-router-dom'
 import styles from './NavBar.module.scss'
 
 const NavBar = () => {
-  const links = [
-    {
-      id:"home",
-      toLink: "/",
-      name: "Home"
-    },
-    {
-      id:"favorite",
-      toLink: "/favorite",
-      name: "Favorite"
-    },
-    {
-      id:"about",
-      toLink: "/about",
-      name: "About"
-    }
-  ];
-
   return (
     <nav className={styles.nav}>
       <div>
         <Link to="/"><i className="fa fa-tasks" aria-hidden="true"></i></Link>
       </div>
       <ul className={styles.list}>
-        {links.map(link => 
-        <li>
-          <NavLink 
-            key={link.id} 
-            className={({isActive}) => isActive ? styles.linkActive : undefined} 
-            to={link.toLink}>
-              {link.name}
-          </NavLink>
-        </li>)}
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/favorite">Favorite</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
       </ul>
     </nav>
   );
